4db4523d633e59677bbf00b50d2a16a0cc2d2f02
Waleed <wsaleh1991@gmail.com>
2021-05-17 00:54:00 -0500

    added text to about me and landing page as well as some animations for languages.

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export const Landing = () => {
-    return (
-        <div>
-            <h1>Welcome to my Home page</h1>
-        </div>
-    )
-}
+	return (
+		<div>
+			<h1> Welcome to my Home page </h1>
+			<div className='intro'>
+				<p className='intro-body'>
+					Hello and welcome to my portfolio where you will learn about my
+					journey into the coding world. Here you learn about what languages I
+					have learned in the past 3 years and what I hope to learn in the
+					future as well. So buckle up and I hope you enjoy the site!Feedback is
+					greatly appreciated as well! Thank you!
+				</p>
+			</div>
+		</div>
+	);
+};
